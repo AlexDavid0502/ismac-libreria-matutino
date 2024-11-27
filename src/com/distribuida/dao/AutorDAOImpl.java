a24a1cf8137e1ca2431a77d86926d118488b4afd
alex <alex@LAB-SW3-06.ALUMNOS.LOCAL>
2024-11-27 10:16:38 -0500

    Clase

---
+++ src/com/distribuida/dao/AutorDAOImpl.java
@@ -0,0 +1,10 @@
+package com.distribuida.dao;
+
+public class AutorDAOImpl {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/com/distribuidaprincipaldao/ClienteDAOPrincipal.java
+++ src/com/distribuidaprincipaldao/ClienteDAOPrincipal.java
@@ -15,10 +15,32 @@ public class ClienteDAOPrincipal {
 		
 		ClienteDAO clienteDAO = context.getBean("clienteDAOImpl", ClienteDAO.class);
 		
+		//Insert 
+		Cliente cliente = new Cliente(0,"1727353930","Juan","Taipe","Quito","0987562247","jtaipe@correo.com");
+		//clienteDAO.add(cliente);
+		
+		//update
+		Cliente cliente2 = new Cliente(39,"1721353930","David","Cardenas","Quito","0988762247","davidc@correo.com");
+		//clienteDAO.up(cliente2);
+		
+		//findOne
+		try {
+			Cliente cliente3 = clienteDAO.findOne(39);
+			System.out.println(cliente3.toString());
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}		
+		// delete
+		//clienteDAO.del(39);
+		
+		
+		
+		//findAll
 		List<Cliente> clientes = clienteDAO.findAll();
 		
-		for (Cliente cliente : clientes) {
-			System.out.println(cliente.toString());
+		for (Cliente item : clientes) {
+			System.out.println(item.toString());
 		}
 		
 		context.close();
